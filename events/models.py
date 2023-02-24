a0ec8e449e84da312ca201a1f552e67eb90cf1fc
cheshta1828 <cheshtaabatra1828@gmail.com>
2023-02-24 11:59:07 +0530

    event model

---
+++ events/models.py
@@ -0,0 +1,24 @@
+import uuid
+from django.db import models
+from accounts.models import NGO
+CHOICES = [
+        ('1', 'ONLINE'),
+        ('2', 'OFFLINE'),
+        ]
+
+# Create your models here.
+class events(models.Model):
+    e_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    e_name = models.CharField(max_length=200)
+    type=models.CharField(
+        max_length=10,
+        choices=CHOICES,
+        default=2
+    )
+    start_date=models.DateField("startdate(mm/dd/yyyy)",auto_now_add=False,auto_now=False,blank=True)
+    end_date=models.DateField("enddate(mm/dd/yyyy)",auto_now_add=False,auto_now=False,blank=True)
+    nature = models.CharField(max_length=200)
+    noofvolunteers = models.IntegerField()
+    id = models.ForeignKey(NGO,on_delete=models.CASCADE)
+
+    
