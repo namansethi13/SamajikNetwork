0756775688633048ecd9711d01c7a3bf29aa9537
stinger13 <93597591+stinger13@users.noreply.github.com>
2023-02-24 11:08:07 +0530

    added user models to admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import BaseUser
+from .models import NGO
+from .models import Volunteer
+
+
+class UserAdminConfig(UserAdmin):
+    model = BaseUser
+
+    search_fields = ("username", "email")
+    list_filter = ("username", "email", "is_active", "is_superuser")
+
+    ordering = ("-create_date",)
+
+    list_display = ("email","is_active", "is_staff")
+
+    readonly_fields = ("create_date",)
+
+    fieldsets = (
+        (None, {"fields" : ["email"]}),
+        ("Verifications", {"fields" : ("is_email_verified",)}),
+        ("Permisssions", {"fields" : ("is_active","is_staff", "is_superuser", "groups",)}),   
+    )
+
+    add_fieldsets = (
+        (None, 
+            {
+                "classes" : ("wide", ), 
+                "fields" : (
+                    # "username",
+                    "email", 
+                    "password1",
+                    "password2", 
+                    "is_active",
+                    "is_staff",
+                ),
+            },
+        ),
+    )
+
+admin.site.register(BaseUser, UserAdminConfig)
+
+admin.site.register(NGO)
+admin.site.register(Volunteer)
