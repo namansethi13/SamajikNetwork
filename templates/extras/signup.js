248186da00955de88e2ae0930c1b1ba67a911e22
Alan <alansijok@gmail.com>
2023-02-25 23:37:19 +0530

    extra files

---
+++ templates/extras/signup.js
@@ -0,0 +1,10 @@
+var d=document.querySelector('#id_role')
+var value= d.options[d.selectedIndex].value;
+console.log(value)
+var e= document.querySelectorAll('')
+e.forEach(element => {
+    element.addEventListener("click",()=>{
+        console.log(element.value)
+    })
+    
+});
