49e849bedbafd249848009719c6857733c3a64f2
namansethi13 <93597591+namansethi13@users.noreply.github.com>
2023-02-26 00:46:12 +0530

    chacha

---
+++ templates/samajikmediaFrontend/signup.js
@@ -0,0 +1,49 @@
+// Get the radio buttons
+const radioButtons = document.getElementsByName('radios');
+const selectElement = document.getElementById('id_role');
+
+// Add event listener to each radio button
+radioButtons.forEach((radioButton) => {
+  radioButton.addEventListener('change', (event) => {
+    // Log the selected radio button's value
+    selectElement.value = event.target.id;
+  });
+});
+
+const radioNGO = document.getElementById("NGO");
+  const radioVolunteer = document.getElementById("VOLUNTEER");
+  const inputsNGO = document.querySelectorAll(".NGO");
+  const inputsVolunteer = document.querySelectorAll(".Volunteer");
+  const labelsNGO = document.querySelectorAll(".NGO");
+  const labelsVolunteer = document.querySelectorAll(".Volunteer");
+
+  // Add an event listener to the radio buttons
+  radioNGO.addEventListener("click", function() {
+    // Hide the Volunteer inputs/labels
+    inputsVolunteer.forEach(input => input.style.display= "none" );
+    labelsVolunteer.forEach(label => label.style.display= "none");
+
+    // Show the NGO inputs/labels
+    inputsNGO.forEach(input => input.style.display= "inline");
+    labelsNGO.forEach(label => label.style.display= "inline");
+    const labelFirstName = document.querySelector("label[for='id_first_name']");
+    if (labelFirstName) {
+      labelFirstName.innerHTML = "Organization name:";
+    }
+  });
+
+  radioVolunteer.addEventListener("click", function() {
+    // Hide the NGO inputs/labels
+    inputsNGO.forEach(input => input.style.display= "none");
+    labelsNGO.forEach(label => label.style.display= "none");
+
+    // Show the Volunteer inputs/labels
+    inputsVolunteer.forEach(input => input.style.display= "inline");
+    labelsVolunteer.forEach(label => label.style.display= "inline");
+
+    const labelFirstName = document.querySelector("label[for='id_first_name']");
+    if (labelFirstName) {
+      labelFirstName.innerHTML = "First name:";
+    }
+
+  });
