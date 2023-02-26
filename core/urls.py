526a6b087206ca55e11ae891b616f0e7404050c5
Alan <alansijok@gmail.com>
2023-02-27 01:01:21 +0530

    deploy try 1

--- core/urls.py
+++ core/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path , include
 from .views import homeview
@@ -25,3 +28,6 @@ urlpatterns = [
 
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
